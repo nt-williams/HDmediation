df2bdd71cb08428ab7c0b3620315432c3ef012fe
Nick Williams <ntwilliams.personal@gmail.com>
2022-05-05 11:33:57 -0700

    fixing bug in e and g regressions. adding b regression.

---
+++ tmce/R/b.R
@@ -0,0 +1,15 @@
+b <- function(data, npsem, family, folds, learners) {
+    b <- matrix(nrow = nrow(data), ncol = 2)
+    colnames(b) <- c("0", "1")
+    for (v in seq_along(folds)) {
+        train <- origami::training(data, folds[[v]])
+        valid_1 <- valid_0 <- origami::validation(data, folds[[v]])
+        valid_1[[npsem$A]] <- 1
+        valid_0[[npsem$A]] <- 0
+
+        preds <- crossfit(train, list(valid_0, valid_1), npsem$Y, npsem$history("Y"), family, learners)
+        b[folds[[v]]$validation_set, 1] <- preds[[1]]
+        b[folds[[v]]$validation_set, 2] <- preds[[2]]
+    }
+    b
+}
