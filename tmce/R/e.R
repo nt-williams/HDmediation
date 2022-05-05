7470be2ef28805656f8b435863dc94cae8456a41
Nick Williams <ntwilliams.personal@gmail.com>
2022-05-05 11:21:02 -0700

    e regression.

---
+++ tmce/R/e.R
@@ -0,0 +1,13 @@
+e <- function(data, npsem, folds, learners) {
+    e <- matrix(nrow = nrow(data), ncol = 1)
+    for (t in 1:length(npsem$A)) {
+        for (v in seq_along(folds)) {
+            train <- origami::training(data, folds[[v]])
+            valid <- origami::validation(data, folds[[v]])
+
+            e[folds[[v]]$validation_set, 1] <-
+                crossfit(train, list(valid), npsem$A, c(npsem$M, npsem$W), "binomial", learners)[[1]]
+        }
+    }
+    e
+}
