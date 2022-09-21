7070b6edd6f67c9434fc03ccffd9f32f4cbd18bd
nt-williams <ntwilliams.personal@gmail.com>
2022-09-21 09:58:38 -0600

    cross fit with SL

--- tmce/R/crossfit.R
+++ tmce/R/crossfit.R
@@ -1,34 +1,26 @@
-crossfit <- function(train, valid, y, x, type = c("binomial", "gaussian"), id = NULL) {# , learners) {
-    fit <- regress(train, y, x, id, match.arg(type))
+crossfit <- function(train, valid, y, x, type = c("binomial", "gaussian"), 
+                     id = NULL, learners = c("SL.mean", "SL.glm", "SL.glm.interaction", "SL.xgboost", "SL.earth")) {
+    fit <- regress(train, y, x, id, match.arg(type), learners = learners)
     lapply(valid, function(newX) predictt(fit, newX[, x]))
 }
 
-regress <- function(train, y, x, id, type) { #, learners) {
+regress <- function(train, y, x, id, type, learners) {
     if (!is.null(id)) {
         id <- train[, id]
     }
-
-    fit <- glmnet3(train[, x], train[[y]], id = id, family = type)
-
-    # fit <- sal(train[, x], train[[y]], id = id, family = type, ratio = 0.2)
-
-    # family <- ifelse(type == "binomial", binomial(), gaussian())
-    # fit <- SuperLearner::SuperLearner(
-    #     train[[y]], train[, x], family = family[[1]], SL.library = learners, id = id,
-    #     method = "method.NNLS", env = environment(SuperLearner::SuperLearner)
-    # )
     
-    # bart_data <- cbind(Y = train[[y]], train[, x])
-    # form <- Y ~ .
-    # 
-    # fit <- dbarts::bart2(formula = form, data = bart_data, keepTrees = TRUE, verbose = FALSE)
+    # fit <- glmnet3(train[, x], train[[y]], id = id, family = type)
+
+    family <- ifelse(type == "binomial", binomial(), gaussian())
+    fit <- SuperLearner::SuperLearner(
+        train[[y]], train[, x], family = family[[1]], SL.library = learners, id = id,
+        method = "method.NNLS", env = environment(SuperLearner::SuperLearner)
+    )
+    
     fit
 }
 
 predictt <- function(fit, newX) {
-    predict.glmnet3(fit, newX)
-    # predict.sal(fit, newX)
-    # predict(fit, data[, fit$varNames])$pred[, 1]
-    # mat <- predict(fit, newX, type = "response")
-    # apply(mat, 2, mean)
+    # predict.glmnet3(fit, newX)
+    predict(fit, newX)$pred[, 1]
 }
