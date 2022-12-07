262529c97eaf18b48524078d7260cf1961ff124d
nt-williams <ntwilliams.personal@gmail.com>
2022-12-07 10:15:54 -0800

    Removing extra dependency, small bug fix.

--- _research/results.R
+++ _research/results.R
@@ -1,31 +1,32 @@
 suppressPackageStartupMessages({
     library(tidyverse)
     library(kableExtra)
+    
 })
 
-transport <- F
+transport <- T
 if (transport) {
-    dgp1t <- readRDS(glue("_research/data/summary_transported_TRUE_3.rds"))
-    dgp2t <- readRDS(glue("_research/data/summary_transported_TRUE_2.rds"))
-    dgp1f <- readRDS(glue("_research/data/summary_transported_FALSE_3.rds"))
-    dgp2f <- readRDS(glue("_research/data/summary_transported_FALSE_2.rds"))
+    dgp1t <- readRDS("_research/data/summary_transported_TRUE_3.rds")
+    dgp2t <- readRDS("_research/data/summary_transported_TRUE_2.rds")
+    dgp1f <- readRDS("_research/data/summary_transported_FALSE_3.rds")
+    dgp2f <- readRDS("_research/data/summary_transported_FALSE_2.rds")
 } else {
-    dgp1t <- readRDS(glue("_research/data/summary_not_transported_TRUE_3.rds"))
-    dgp2t <- readRDS(glue("_research/data/summary_not_transported_TRUE_2.rds"))
-    dgp1f <- readRDS(glue("_research/data/summary_not_transported_FALSE_3.rds"))
-    dgp2f <- readRDS(glue("_research/data/summary_not_transported_FALSE_2.rds"))
+    dgp1t <- readRDS("_research/data/summary_not_transported_TRUE_3.rds")
+    dgp2t <- readRDS("_research/data/summary_not_transported_TRUE_2.rds")
+    dgp1f <- readRDS("_research/data/summary_not_transported_FALSE_3.rds")
+    dgp2f <- readRDS("_research/data/summary_not_transported_FALSE_2.rds")
 }
 
 filter(dgp2f, estimand == "direct") |>
     bind_rows(filter(dgp2t, estimand == "direct"), 
               filter(dgp1f, estimand == "direct"), 
-              filter(dgp2t, estimand == "direct")) |>
+              filter(dgp1t, estimand == "direct")) |>
     select(-estimand) |>
     bind_cols({
         filter(dgp2f, estimand == "indirect") |>
             bind_rows(filter(dgp2t, estimand == "indirect"), 
                       filter(dgp1f, estimand == "indirect"), 
-                      filter(dgp2t, estimand == "indirect")) |>
+                      filter(dgp1t, estimand == "indirect")) |>
             select(-estimand, -n) 
     }) |> 
     kbl("latex", booktabs = TRUE,
