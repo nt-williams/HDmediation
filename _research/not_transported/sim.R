06bb3594634acd5d06cef63f966c0e5cb893d7ec
nt-williams <ntwilliams.personal@gmail.com>
2023-09-12 09:13:17 -0700

    Reverting non transported sim control file back to Super learner

--- _research/not_transported/sim.R
+++ _research/not_transported/sim.R
@@ -1,61 +1,47 @@
-# READ ME -----------------------------------------------------------------
-#
-#       Author: Nick Williams
-#       Created: 2023-08-22
-# 
-# -------------------------------------------------------------------------
+suppressPackageStartupMessages({
+    library(HDmediation)
+    library(glue)
+    library(tidyverse)
+})
 
-# 1: gendata.R is continuous M and Z
-# 2: gendata2.R is binary M and Z
+# gendata.R is multivariate M and Z
+# gendata2.R is binary M and Z
 dgp <- 1
+if (dgp == 1) {
+    source("_research/not_transported/gendata3.R")
+} else {
+    source("_research/not_transported/gendata2.R")
+}
 
 # Fit with partial TMLE or not
-tmle <- TRUE
+tmle <- T
 
-library(HDmediation)
-library(glue)
-library(dplyr)
-library(purrr)
-library(future)
+# source("_research/SL.lightgbm.R")
+source("_research/SL.glm.saturated.R")
+source("_research/SL.glmnet3.R")
 
 id <- Sys.getenv("SGE_TASK_ID")
 if (id == "undefined" || id == "") id <- 1
 
 if (dgp == 1) {
-    source("_research/not_transported/gendata.R")
+    learners <- c("SL.glm.interaction", "SL.glm.saturated", "SL.glmnet3", "SL.glm")
+    folds <- 5
 } else {
-    source("_research/not_transported/gendata2.R")
+    learners <- "SL.glm.saturated"
+    folds <- 1
 }
 
-learners <- list("mean", "glm", "earth",
-                 list("lightgbm",
-                      min_data_in_leaf = 1,
-                      num_iterations = 500,
-                      learning_rate = 0.05,
-                      max_bin = 10,
-                      id = "lgbm1"),
-                 list("lightgbm",
-                      min_data_in_leaf = 1,
-                      num_iterations = 1000,
-                      learning_rate = 0.025,
-                      max_bin = 10,
-                      id = "lgbm2"),
-                 list("ranger", num.trees = 500, id = "ranger1"),
-                 list("ranger", num.trees = 1000, id = "ranger2"))
-
 res <- map_dfr(c(500, 1000, 5000, 1e4), function(n) {
-    dat <- sample_data(n)
+    dat <- gendata(n)
     
-    folds <- case_when(n == 500 ~ 10, 
-                       n == 1000 ~ 10, 
-                       n == 5000 ~ 4, 
-                       n == 1e4  ~ 2)
+    z <- names(dat)[startsWith(names(dat), "Z")]
+    m <- names(dat)[startsWith(names(dat), "M")]
     
-    mediation(dat, "a", "w", "z", "m", "y", 
+    mediation(dat, "A", "W1", 
+              z, m, "Y", 
               family = "binomial", 
-              folds = folds, 
-              partial_tmle = tmle,
-              learners_g = learners, 
+              folds = folds, partial_tmle = tmle,
+              learners_g = "SL.mean", 
               learners_c = learners,
               learners_b = learners, 
               learners_e = learners, 
@@ -66,11 +52,12 @@ res <- map_dfr(c(500, 1000, 5000, 1e4), function(n) {
               learners_vbar = learners)
 }, .id = "n")
 
-res <- mutate(res, n = case_when(
-    n == 1 ~ 500, 
-    n == 2 ~ 1000, 
-    n == 3 ~ 5000, 
-    n == 4 ~ 1e4
-))
+res <- mutate(res, 
+              n = case_when(
+                  n == 1 ~ 500, 
+                  n == 2 ~ 1000, 
+                  n == 3 ~ 5000, 
+                  n == 4 ~ 1e4
+              ))
 
 saveRDS(res, glue("_research/data/sim_not_transported_{tmle}_{dgp}_{id}.rds"))
