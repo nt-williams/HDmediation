b2a4e6cfb8bac3a8d05ae6fe6e4477f492fb9455
nt-williams <ntwilliams.personal@gmail.com>
2023-09-12 09:07:33 -0700

    Updated summary of results

--- _research/results.R
+++ _research/results.R
@@ -4,12 +4,14 @@ suppressPackageStartupMessages({
     
 })
 
-transport <- T
+transport <- TRUE
 if (transport) {
     dgp1t <- readRDS("_research/data/summary_transported_TRUE_3.rds")
     dgp2t <- readRDS("_research/data/summary_transported_TRUE_2.rds")
+    dgp3t <- readRDS("_research/data/summary_transported_TRUE_cont.rds")
     dgp1f <- readRDS("_research/data/summary_transported_FALSE_3.rds")
     dgp2f <- readRDS("_research/data/summary_transported_FALSE_2.rds")
+    dgp3f <- readRDS("_research/data/summary_transported_FALSE_cont.rds")
 } else {
     dgp1t <- readRDS("_research/data/summary_not_transported_TRUE_3.rds")
     dgp2t <- readRDS("_research/data/summary_not_transported_TRUE_2.rds")
@@ -20,13 +22,17 @@ if (transport) {
 filter(dgp2f, estimand == "direct") |>
     bind_rows(filter(dgp2t, estimand == "direct"), 
               filter(dgp1f, estimand == "direct"), 
-              filter(dgp1t, estimand == "direct")) |>
+              filter(dgp1t, estimand == "direct"), 
+              filter(dgp3f, estimand == "direct"), 
+              filter(dgp3t, estimand == "direct")) |>
     select(-estimand) |>
     bind_cols({
         filter(dgp2f, estimand == "indirect") |>
             bind_rows(filter(dgp2t, estimand == "indirect"), 
                       filter(dgp1f, estimand == "indirect"), 
-                      filter(dgp1t, estimand == "indirect")) |>
+                      filter(dgp1t, estimand == "indirect"), 
+                      filter(dgp3f, estimand == "indirect"), 
+                      filter(dgp3t, estimand == "indirect")) |>
             select(-estimand, -n) 
     }) |> 
     kbl("latex", booktabs = TRUE,
